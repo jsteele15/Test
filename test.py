1663706015295c1e206811866f83af3df7784c97
jsteele15 <128925976+jsteele15@users.noreply.github.com>
2023-09-07 15:10:01 +0100

    Sponge Bob Case
    This adds a method to the class that makes the words Go LiKe ThIs.

--- test.py
+++ test.py
@@ -8,7 +8,20 @@ class StayClassy():
         for i in self.to_print:
             for v in i:
                 print(v)
-            
+                
+    def Sponge_Case(self):
+        result = ""
+        for i in self.to_print:
+            for v in i:
+                for q in range(len(v)):
+                    if q % 2 == 0:
+                        result += v[q].upper()
+                    else:
+                        result += v[q].lower()
+                        
+        print(result)    
 words = StayClassy("Hello", "world", "jam", "boner")
 
 words.Print_All()            
+
+words.Sponge_Case()
