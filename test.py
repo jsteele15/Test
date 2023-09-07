6399fc17d2e90432e48e897a4a2efa308dde907b
jsteele15 <128925976+jsteele15@users.noreply.github.com>
2023-09-07 14:59:51 +0100

    Class to print hello world
    this commit adds a class that allows you to print multiple items at once.

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 print("hello world")
+
+class StayClassy():
+    def __init__(self, *text):
+        self.to_print = [text]
+    
+    def Print_All(self):
+        for i in self.to_print:
+            for v in i:
+                print(v)
+            
+words = StayClassy("Hello", "world", "jam", "boner")
+
+words.Print_All()            
